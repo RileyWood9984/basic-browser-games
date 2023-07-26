db0adee38b457b588c9b3f94c401d787dac94ec3
RileyWood9984 <rjw9984@gmail.com>
2023-07-25 21:28:35 -0600

    piece meal

--- fighting-game/js/classes.js
+++ fighting-game/js/classes.js
@@ -137,7 +137,7 @@ class Fighter extends Sprite {
 
     switchSprite(sprite) {
         if(this.image === this.sprites.death.image){
-
+            if(this.framesCurrent === this.sprites.death.framesMax-1)
             return
         }
         if (this.image === this.sprites.attack1.image &&
